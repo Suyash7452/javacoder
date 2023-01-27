e2549092cc6c340f04fad2ace46bee4b600840f0
suyash <yadavss7452@gmail.com>
2023-01-27 23:00:04 +0530

    add title

--- Study/src/com/suyash/Main.java
+++ Study/src/com/suyash/Main.java
@@ -9,15 +9,7 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-/*        List<Integer> l1=Arrays.asList(1,4,55,76,89,78);
-        Collections.sort(l1,Collections.reverseOrder());
-        l1.stream().collect(Collectors.toList()).forEach(System.out::println);
-        System.out.println(l1);
-        System.out.println("Even  number from list ="+l1.stream().filter(s-> s%2==0).collect(Collectors.toList()));
-        System.out.println(l1.stream().skip(2).limit(3).collect(Collectors.toList()));
-        System.out.println(l1.stream().count());
-        System.out.println(l1.stream().max(Integer::compareTo).get());*/
-
+//simple calculator java code
         double num1,num2;
         char operater ;
         Scanner scanner=new Scanner(System.in);
